9782a2872f257b86583283833d970d4f57451615
Alfonso Ruiz <1800ruiz@gmail.com>
2019-06-25 18:17:20 -0700

    Setup redux-logger

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
 import { reducer } from './reducers';
 
 import App from './App';
 
 // Creating redux store
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
     // Provider wraps application to allow store to be distributed throughout UI of app
